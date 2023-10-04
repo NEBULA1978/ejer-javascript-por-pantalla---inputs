ad303fca573feec42947501a202be0b39cb459df
NEBULA1978 <rpascualey@gmail.com>
2023-10-04 23:31:58 +0200

    Repasando scripting bash sv4 comentando empezamos de cero menu basico ejecucion segunda funcion

--- blockchainbahs/btanalizer.sh
+++ blockchainbahs/btanalizer.sh
@@ -3,7 +3,7 @@
 # Author: Marcelo Vázquez (aka S4vitar)
 # Modifications to PyBlock (pip3 install pybitblock)
 
-#Colours
+# Colours
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
 redColour="\e[0;31m\033[1m"
@@ -23,8 +23,6 @@ trap ctrl_c INT
 # Funciones
 function ctrl_c() {
   echo -e "\n${redColour}[!] Saliendo...\n${endColour}"
-
-  # rm ut.t* money* total_entrada_salida.tmp entradas.tmp salidas.tmp bitcoin_to_dollars 2>/dev/null
   tput cnorm
   exit 1
 }
@@ -36,39 +34,33 @@ function helpPanel(){
 	echo -e "\t\t${purpleColour}unconfirmed_transactions${endColour}${yellowColour}:\t Listar transacciones no confirmadas${endColour}"
 	echo -e "\t\t${purpleColour}inspect${endColour}${yellowColour}:\t\t\t Inspeccionar un hash${endColour}"
 	echo -e "\t\t${purpleColour}address${endColour}${yellowColour}:\t\t\t Inspeccionar una dirección${endColour}"
-	# echo -e "\n\t${grayColour}[-n]${endColour}${yellowColour} Limitar el número de resultados${endColour}${blueColour} (Ejemplo: -n 10)${endColour}"
-	# echo -e "\n\t${grayColour}[-i]${endColour}${yellowColour} Proporcionar el hash de transacción${endColour}${blueColour} (Ejemplo: -i 000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f)${endColour}"
-	# echo -e "\n\t${grayColour}[-a]${endColour}${yellowColour} Proporcionar la dirección de transacción${endColour}${blueColour} (Ejemplo: -a 1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa)${endColour}"
-	# echo -e "\n\t${grayColour}[-h]${endColour}${yellowColour} Mostrar este panel de ayuda${endColour}\n"
+	echo -e "\n\t${grayColour}[-h]${endColour}${yellowColour} Mostrar este panel de ayuda${endColour}\n"
 
 	tput cnorm; exit 1
 }
 
+function unconfirmed_transactions(){
+  echo "Listing unconfirmed transactions"
+  # Add code here to list unconfirmed transactions
+}
 
-# Funciones
-
-
-
-parameter_counter=0;while getopts "e:h:" arg; do
-	case $arg in
-		e) exploration_mode=$OPTARG; let parameter_counter+=1;;
-		# n) number_output=$OPTARG; let parameter_counter+=1;;
-		# i) inspect_transaction=$OPTARG; let parameter_counter+=1;;
-		# a) inspect_address=$OPTARG; let parameter_counter+=1;;
-		h) helpPanel;;
-	esac
+# Main script
+parameter_counter=0
+while getopts "e:h:" arg; do
+  case $arg in
+    e) exploration_mode="$OPTARG"; let parameter_counter+=1;;
+    h) helpPanel;;
+  esac
 done
 
 tput civis
 
 if [ $parameter_counter -eq 0 ]; then
-	helpPanel
+  helpPanel
 else
-  echo "Hola"
+  if [ "$(echo $exploration_mode)" == "unconfirmed_transactions" ]; then
+    unconfirmed_transactions
+  fi
 fi
 
-# operaciones sumar numeros en bash para hashes:
-# echo $((2+5))
-# echo "2+5" | bc
-
-# Voy minuto 23:43
+# Add more exploration modes and functions as needed
