11b7c6321d56ec4d8f3e9fa16732cdc7dbaeeb82
NEBULA1978 <rpascualey@gmail.com>
2023-10-06 01:55:22 +0200

    Reapasando web scraping pinguino de mario min 14

--- scrapping-web/scraping2.sh
+++ scrapping-web/scraping2.sh
@@ -6,5 +6,4 @@ curl https://www.vulnhub.com/ > log.log  2>/dev/null
 
 echo "Estas son las maquinas disponibles: "
 
-cat log.log | grep 'href="/entry/' | 
-tr -d '#' | sed 's/entry/ /' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /'
+cat log.log | grep 'href="/entry/' | tr -d '#' | sed 's/entry/ /' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /' | sed 's/"/ /' | tr -d '>' | tr -d ' ' | uniq 
