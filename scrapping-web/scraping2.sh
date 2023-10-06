508e8541530276188f35a0a5361bf3f7868750c9
NEBULA1978 <rpascualey@gmail.com>
2023-10-06 02:14:20 +0200

    Reapasando web scraping pinguino de mario finalizado

--- scrapping-web/scraping2.sh
+++ scrapping-web/scraping2.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-
-# Peticion https
-curl https://www.vulnhub.com/ > log.log  2>/dev/null
+# Peticion https descargamos pagina a archivo
+curl https://www.vulnhub.com/ >log.log 2>/dev/null
 
 echo "Estas son las maquinas disponibles: "
 
-cat log.log | grep 'href="/entry/' | tr -d '#' | sed 's/entry/ /' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /' | sed 's/"/ /' | tr -d '>' | tr -d ' ' | uniq 
+cat log.log | grep 'href="/entry/' | tr -d '#' | sed 's/entry/ /' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /' | sed 's/"/ /' | tr -d '>' | tr -d ' ' | uniq
+
+# Buscamos si se encuentra esta maquina dentro del archivo log.log descargado:
+# Si añaden una nueva y pasa a la segunda pagina  no la detrecta y nos dice hay maquina nueva!!! 
+cat log.log | grep noob >/dev/null
+
+# Evaluamos que el comando anterior se ejecuto correctamente con $?
+if [ "$(echo $?)" == "0" ]; then
+  echo "No se encuentra ninguna maquina nueva"
+else
+  echo "Hay maquina nueva!!!"
+
+fi
+
+# No queremos tener basura
+rm log.log
