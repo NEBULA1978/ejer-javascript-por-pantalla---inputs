59200a6e289c03e6f78039d8b867b9589bc3a766
NEBULA1978 <rpascualey@gmail.com>
2023-10-06 03:09:21 +0200

    Reapasando web scraping pinguino de mario finalizado añadiendo ejemplo limpio añado mostrar maquinas y pulsar numero para elegir maquina añado introducir nombre tambien ñado otro ejemplo 2

--- scrapping-web/scraping3-input-nom-machine5.sh
+++ scrapping-web/scraping3-input-nom-machine5.sh
@@ -25,6 +25,7 @@ if [ -f log.log ]; then
   # Verificar si el valor ingresado es un número
   if [[ $input =~ ^[0-9]+$ ]]; then
     machine_number=$input
+    # Verificar si el número ingresado está en el rango válido
     if [ "$machine_number" -ge 0 ] && [ "$machine_number" -lt ${#machines[@]} ]; then
       machine_name="${machines[machine_number]}"
       echo "Seleccionaste la máquina '$machine_name'."
@@ -53,23 +54,3 @@ else
   echo "Archivo log.log creado y poblado."
   echo "Por favor, ejecuta el script nuevamente para buscar máquinas."
 fi
-
-# Este script de Bash realiza las siguientes acciones:
-
-#     Define una función llamada descargar_pagina_temporal que utiliza el comando curl para descargar la página web de VulnHub (https://www.vulnhub.com/) y guarda la descarga en un archivo temporal llamado temporal.log.
-
-#     Verifica si el archivo log.log existe en el directorio actual utilizando la condición [ -f log.log ].
-
-#     Si log.log existe, procede con la búsqueda y muestra las máquinas disponibles en la lista. Esto se hace extrayendo las máquinas disponibles del archivo log.log, procesando los datos y almacenándolos en un array llamado machines. Luego, muestra estas máquinas junto con un número asignado a cada una.
-
-#     Solicita al usuario que ingrese el número o el nombre de la máquina que desea buscar.
-
-#     Verifica si el valor ingresado es un número o un nombre de máquina. Si es un número, verifica si está en el rango válido y busca la máquina correspondiente en machines. Si es un nombre de máquina, busca una coincidencia parcial en el archivo log.log.
-
-#     Informa al usuario si la máquina existe en la lista original en log.log o si no existe.
-
-#     Si el archivo log.log no existe, se ejecuta la función descargar_pagina_temporal para descargar la página web en un archivo temporal temporal.log. Luego, se renombia temporal.log a log.log, creando así un archivo log.log poblado con datos.
-
-#     Se muestra un mensaje indicando que el archivo log.log ha sido creado y poblado y se solicita al usuario que ejecute el script nuevamente para buscar máquinas.
-
-# En resumen, el script permite al usuario buscar máquinas en la lista de VulnHub, descargando la lista si no existe previamente y luego verificando si la máquina especificada por el usuario está presente en la lista o no. Además, muestra las máquinas disponibles para su selección.
